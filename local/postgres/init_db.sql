8eb5c82a5c63ec4f5803542d0af13cf96eec6277
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-05-18 12:03:05 +0300

    save / modify init_db.sql

--- local/postgres/init_db.sql
+++ local/postgres/init_db.sql
@@ -1,12 +1,42 @@
-CREATE SCHEMA IF NOT EXISTS user_service;
+-- Создаём отдельные БД для каждого сервиса
+CREATE DATABASE logistics_user_db;
+CREATE DATABASE logistics_cargo_db;
+CREATE DATABASE logistics_delivery_db;
+CREATE DATABASE logistics_payment_db;
+CREATE DATABASE logistics_transport_db;
+-- CREATE DATABASE logistics_notification_db;
 
--- CREATE SCHEMA IF NOT EXISTS logistics-transport_service;
---
--- CREATE SCHEMA IF NOT EXISTS delivery-transport_service;
---
--- CREATE SCHEMA IF NOT EXISTS cargo-transport_service;
---
--- CREATE SCHEMA IF NOT EXISTS payment-transport_service;
+       -- Создаём схемы в каждой БД (нужно подключаться к каждой БД отдельно)
+-- User Service
+\c logistics_user_db
+CREATE SCHEMA IF NOT EXISTS user_schema;
+GRANT ALL PRIVILEGES ON SCHEMA user_schema TO logistics_user;
+
+-- Transport Service
+\c logistics_transport_db
+CREATE SCHEMA IF NOT EXISTS transport_schema;
+GRANT ALL PRIVILEGES ON SCHEMA transport_schema TO logistics_user;
+
+-- Delivery Service
+\c logistics_delivery_db
+CREATE SCHEMA IF NOT EXISTS delivery_schema;
+GRANT ALL PRIVILEGES ON SCHEMA delivery_schema TO logistics_user;
+
+-- Cargo Service
+\c logistics_cargo_db
+CREATE SCHEMA IF NOT EXISTS cargo_schema;
+GRANT ALL PRIVILEGES ON SCHEMA cargo_schema TO logistics_user;
+
+-- Payment Service
+\c logistics_payment_db
+CREATE SCHEMA IF NOT EXISTS payment_schema;
+GRANT ALL PRIVILEGES ON SCHEMA payment_schema TO logistics_user;
+
+-- Notification Service
+-- \c logistics_notification_db
+-- CREATE SCHEMA IF NOT EXISTS notification_schema;
+-- GRANT ALL PRIVILEGES ON SCHEMA notification_schema TO logistics_user;
 
 -- Дополнительно: назначить права
-GRANT ALL PRIVILEGES ON SCHEMA user_service TO logistics_user;
+\c logistics_user_db
+GRANT ALL PRIVILEGES ON SCHEMA user_schema TO logistics_user;
